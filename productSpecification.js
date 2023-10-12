e59ce04f17e7f6736182cf1fe1ffff50140f7e05
jackitic <fajardo0689@gmail.com>
2023-10-11 23:06:57 -0500

    advances

---
+++ productSpecification.js
@@ -0,0 +1,8 @@
+import back from "./src/components/back/back.js";
+import detail from "./src/components/detail/detail.js";
+import nav from "./src/components/nav/nav.js";
+
+nav()
+back()
+detail()
+//console.log(localStorage.getItem('productModal'));
---
+++ src/components/detail/localStorageCart.js
@@ -0,0 +1,11 @@
+function localStorageCart(size) {
+
+  if (!size) {
+    console.error('No size assigned ❌ ');
+    throw new Error('No size assigned ❌ ')
+  }
+
+
+}
+
+export default localStorageCart
